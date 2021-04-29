726afc8a20d293cfd896bc8b90dcf53dbc4a2b7a
張文浩 <zhangwenhao@sinosoft.hk>
2021-04-29 14:54:01 +0800

    初始化java项目

---
+++ myblog-java/src/main/java/com/waitforflowers/myblogjava/MyblogJavaApplication.java
@@ -0,0 +1,13 @@
+package com.waitforflowers.myblogjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogJavaApplication.class, args);
+    }
+
+}
---
+++ myblog-java/src/test/java/com/waitforflowers/myblogjava/MyblogJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waitforflowers.myblogjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
